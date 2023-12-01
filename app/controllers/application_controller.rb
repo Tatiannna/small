5b112cc098160f60af3b3477a573030beb8195a3
Tatianna <tatirobinson@gmail.com>
2023-12-01 15:01:22 -0800

    Add auth methods to applicatioController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,28 @@
 class ApplicationController < ActionController::API
+
+
+    def current_user
+        @current_user ||= User.find_by(session_token = session[:session_token])
+        # user whose `session_token` == token in `session` cookie
+      end
+      
+      def login!(user)
+        session[:session_token] = current_user.reset_session_token!
+        # reset `user`'s `session_token` and store in `session` cookie
+      end
+      
+      def logout!
+        current_user.reset_session_token!
+        session[:session_token] = nil
+        # reset the `current_user`'s session cookie, if one exists
+        # clear out token from `session` cookie
+        @current_user = nil # so that subsequent calls to `current_user` return nil
+      end
+      
+      def require_logged_in
+        unless current_user
+          render json: { message: 'Unauthorized' }, status: :unauthorized 
+        end
+      end
+      
 end
