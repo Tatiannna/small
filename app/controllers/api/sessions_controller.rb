5322695d31a1005a8aaa4de3f134e3aded40bcd7
Tatianna <tatirobinson@gmail.com>
2023-12-01 15:33:35 -0800

    Edit session controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,10 +1,29 @@
 class Api::SessionsController < ApplicationController
+
+
   def show
+    @user = current_user
+    render json: @user
   end
 
   def create
+    @user = User.find_by_credentials(params[:credential], params[:password])
+    if @user
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: { errors: ['Invalid credentials'] }, 
+        status: :unauthorized
+    end
+
   end
 
   def destroy
+    logout!
+    render json: "logged out successfully"
   end
+
+  private
+
+
 end
