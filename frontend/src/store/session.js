391025aa65da528eefffa0b409dc12929b5d8c54
Tatianna <tatirobinson@gmail.com>
2023-12-02 14:53:26 -0800

    Edit session actions, controller

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -1,62 +1,57 @@
 import csrfFetch from "./csrf";
 
-const RECEIVE_SESSION = '/session/RECEIVE_SESSION';
-const REMOVE_SESSION= '/session/REMOVE_SESSION';
+const SET_USER = '/session/SET_USER';
+const REMOVE_USER = '/session/REMOVE_USER';
 
 
 
-const receiveSession = user => {
-    type: RECEIVE_SESSION,
+const setUser = user => {
+    type: SET_USER,
     user
 }
 
-const removeSession = userId => {
-    type: RECEIVE_SESSION,
-    userId
+const removeUser = () => {
+    type: REMOVE_USER
 }
 
 
 
 
-export const createSession = (user) => async (dispatch) => {
+export const login = ({credential, password}) => async (dispatch) => {
     const res = await csrfFetch('/api/session', {
         method: "POST",
-        body: JSON.stringify(user)
+        body: JSON.stringify({credential, password})
     })
 
     if (res.ok){
         let data = await res.json();
-        dispatch(receiveSession(user))
+        dispatch(setUser(data.user))
     }else{
-        console.log("Something went wrong")
+        throw res;
     }
 }
 
-export const deleteSession = (userId) => async (dispatch) => {
+export const logout = (userId) => async (dispatch) => {
     const res = await csrfFetch('/api/session', {
         method: "DELETE"
     })
 
     if (res.ok){
         // let data = await res.json();
-        dispatch(deleteSession(userId))
+        dispatch(removeUser())
     }else{
         throw res;
     }
 }
 
 
-
-
 const sessionReducer = (state = {}, action) => {
     let newState = {...state}
     switch(action.type){
-        case RECEIVE_SESSION:
-            newState[action.user.id] = action.user;
-            return newState;
-        case REMOVE_SESSION:
-            delete newState[action.userId]
-            return newState;
+        case SET_USER:
+            return {...newState, user: action.user}
+        case REMOVE_USER:
+            return {...newState, user: null}
         default:
             return state;
     }
