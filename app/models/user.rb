90d7be7e7c4ca432936402f36efe59fd357192ee
Tatianna <tatirobinson@gmail.com>
2023-12-03 13:28:40 -0800

    Fix error in current_user()

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,7 +24,7 @@ class User < ApplicationRecord
 
     def self.find_by_credentials(email, password)
         @user = User.find_by(email: email)
-
+        
         if @user && @user.authenticate(password)
             @user
         else
