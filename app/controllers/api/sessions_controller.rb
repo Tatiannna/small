95c9eb1bb09d764366f4c4956357fe911e7ca58d
Tatianna <tatirobinson@gmail.com>
2023-12-03 04:01:51 -0800

    Replace 'credential' with 'email'

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -7,7 +7,8 @@ class Api::SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_credentials(params[:credential], params[:password])
+
+    @user = User.find_by_credentials(params[:email], params[:password])
     if @user
       login!(@user)
       render 'api/users/show'
