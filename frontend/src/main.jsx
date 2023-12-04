94dcabe98620aadab18426bc190337091a170437
Tatianna <tatirobinson@gmail.com>
2023-12-03 22:04:10 -0800

    Initialize() state with current user before rending App

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,12 +4,21 @@ import App from './App';
 import './index.css';
 import  {Provider} from 'react-redux';
 import configureStore from './store/store';
+import { restoreCSRF } from './store/csrf';
 
-const store = configureStore();
 
-if (import.meta.env.MODE !== 'production') {
-  window.store = store;
-}
+
+const initializeApp = () => {
+  const currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+  let initialState;
+  if (currentUser) {
+    initialState = {
+      session: {
+        user: currentUser
+      }
+    }
+  }
+const store = configureStore(initialState);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -19,3 +28,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </Provider>
   </React.StrictMode>
 );
+
+}
+
+restoreCSRF().then(initializeApp);
