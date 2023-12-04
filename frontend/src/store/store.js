64b65204fa929b1c47ba8404ee062d2b4386dfe5
Tatianna <tatirobinson@gmail.com>
2023-12-03 18:30:46 -0800

    Cleanup configureStore() method

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,27 +1,17 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import sessionReducer from './session';
+import logger from 'redux-logger';
 
 
 
 const rootReducer = combineReducers({
     session: sessionReducer
-
 });
 
-let enhancer;
-if (import.meta.env.MODE === 'production') {
-  enhancer = applyMiddleware(thunk);
-} else {
-  const logger = (await import("redux-logger")).default;
-  const composeEnhancers =
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  enhancer = composeEnhancers(applyMiddleware(thunk, logger));
-}
 
-
-const configureStore = (preloadedState) => {
-    return createStore(rootReducer, preloadedState, enhancer);
+const configureStore = (initialState = {}) => {
+    return createStore(rootReducer, initialState, applyMiddleware(thunk, logger));
 };
 
 export default configureStore;
