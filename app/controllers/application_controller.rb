c6de61e8b897fd04e1535a283c1e6a3d9aa1aa8c
Tatianna <tatirobinson@gmail.com>
2023-12-03 18:26:00 -0800

    Fix error in current_user()

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::API
 
 
   def current_user
-      @current_user ||= User.find_by(session_token = session[:session_token])
+      @current_user ||= User.find_by(session_token: session[:session_token])
   end
       
   def login!(user)
@@ -20,7 +20,7 @@ class ApplicationController < ActionController::API
   end
   
   def logout!
-    
+
     current_user.reset_session_token!
     session[:session_token] = nil
     @current_user = nil 
