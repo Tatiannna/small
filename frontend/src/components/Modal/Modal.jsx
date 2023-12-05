54359f3ebad52ec22001afabe753e030c545dd49
Tatianna <tatirobinson@gmail.com>
2023-12-05 12:15:40 -0800

    Edit format, styling of login/signup errors

--- frontend/src/components/Modal/Modal.jsx
+++ frontend/src/components/Modal/Modal.jsx
@@ -13,6 +13,11 @@ const Modal = (props) => {
   const [errors, setErrors] = useState({});
 
 
+  const formattedErrors = () => {
+    let errorsArray;
+    //for 
+  }
+
   const handleSubmit = (e) => {
 
       e.preventDefault();
@@ -28,22 +33,29 @@ const Modal = (props) => {
     return (
         <div className="modal-overlay">
           <div className="modal">
-          <p id="x" onClick={() => props.closeModal()}> &#215; </p>
+            <p id="x" onClick={() => props.closeModal()}> &#215; </p>
+
             { props.formType === "login" ? <p> Welcome back. </p> : <p> Join Small</p>}
-              <form onSubmit={handleSubmit}>
-                <div className="container">
-                    <input type="text" placeholder="Email" onChange={e => setEmail(e.target.value) } />
-                    { props.formType === "signup" && 
-                      <input type="text" 
-                        placeholder="Username" 
-                        onChange={e => setUsername(e.target.value) } 
-                      />}
-                    <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
-                    
-                    {Object.values(errors).map((err, idx) => ( <p key={idx}>{err}</p> ))}
-                    <button>Continue</button>
-                </div>
-              </form>
+
+            <div className="div-errors">
+              {errors['email'] && <p className="error" > {`email ${errors['email']}`}</p>}
+              {errors['username'] && <p className="error" > {`username ${errors['username']}`}</p>}
+              {errors['password'] && <p className="error" > {`password ${errors['password']}`}</p>}
+              {errors['errors'] && <p className="error" > {errors['errors']}</p>}
+            </div>
+            
+            <form onSubmit={handleSubmit}>
+              <div className="container">
+                  <input type="text" placeholder="Email" onChange={e => setEmail(e.target.value) } />
+                  { props.formType === "signup" && 
+                    <input type="text" 
+                      placeholder="Username" 
+                      onChange={e => setUsername(e.target.value) } 
+                    />}
+                  <input type="password" placeholder="Password" onChange={e => setPassword(e.target.value)} />
+                  <button>Continue</button>
+              </div>
+            </form>
           </div>
         </div>
     );
