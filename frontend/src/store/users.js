d6607ec53fa4944582e962ccefd89d8866ae7fa0
Tatianna <tatirobinson@gmail.com>
2023-12-04 18:38:57 -0800

    Create remove user action

--- frontend/src/store/users.js
+++ frontend/src/store/users.js
@@ -3,22 +3,27 @@ import { login } from "./session";
 
 
 const RECEIVE_USER = "/user/ADD_USER";
+const DELETE_USER = "/user/DELETE_USER";
 
-const receiveUser = (user) => {
+
+export const receiveUser = (user) => {
     return {
         type: RECEIVE_USER,
         user
     }
 }
 
+export const deleteUser = () => {
+    return {
+        type: DELETE_USER
+    }
+}
 
 export const createUser = ({email, username, password}) => async (dispatch) => {
     const res = await csrfFetch('/api/users', {
         method: 'POST',
         body: JSON.stringify({email, username, password})
     })
-
-    console.log(res)
 
     if (res.ok){
         let data = await res.json();
@@ -28,11 +33,21 @@ export const createUser = ({email, username, password}) => async (dispatch) => {
     }
 }
 
+
+// export const deleteAccount = () => async (dispatch) => {
+//     if (res.ok){
+//         let data = await res.json();
+//         dispatch(removeUser(data));
+//     }
+// }
+
 const userReducer = (state = {}, action) => {
     let newState = {...state}
     switch (action.type){
         case RECEIVE_USER:
             return {...newState, user: action.user};
+        case DELETE_USER:
+            return {...newState, user: null}
         default:
             return state;
     }
