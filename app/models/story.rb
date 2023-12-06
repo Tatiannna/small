e16b82c21ab447e1978b005bdb1813589b79bb8c
Tatianna <tatirobinson@gmail.com>
2023-12-05 17:30:11 -0800

    Create Stories table, model

---
+++ app/models/story.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: stories
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  author_id  :bigint           not null
+#  body       :text             not null
+#  topic_id   :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+class Story < ApplicationRecord
+
+    validates :author_id, :topic_id, :title, :body, presence: true
+
+    belongs_to :author
+
+    belongs_to :topic
+
+end
