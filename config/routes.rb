328b40466049077508944cd7dbbab9c9fc2dcf02
Tatianna <tatirobinson@gmail.com>
2023-12-05 22:58:26 -0800

    Setup /stories routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,10 @@ Rails.application.routes.draw do
   namespace :api, defaults: { format: :json } do
     resources :users, only: :create
     resource :session, only: [:show, :create, :destroy]
+    resources :stories
   end
+
+
 
   get '*path',
     to: 'static_pages#frontend',
