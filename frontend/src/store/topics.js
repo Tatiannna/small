7acc308ce964e0f83821217a926a73f78eb297db
Tatianna <tatirobinson@gmail.com>
2023-12-06 10:22:04 -0800

    Create Topic actions, action creators, reducer

---
+++ frontend/src/store/topics.js
@@ -0,0 +1,67 @@
+import csrfFetch from "./csrf";
+
+const RECEIVE_TOPICS = '/topics/RECEIVE_TOPICS';
+const RECEIVE_TOPIC = '/tpoics/RECEIVE_TOPIC';
+const REMOVE_TOPIC = '/tpoics/REMOVE_TOPIC';
+
+
+const receiveTopics = () => {
+    return {
+        type: RECEIVE_TOPICS,
+        topics
+    }
+}
+const receiveTopic = (topic) => {
+    return {
+        type: RECEIVE_TOPIC,
+        topic
+    }
+}
+
+const removeTopic = (id) => {
+    return {
+        type: REMOVE_TOPIC,
+        id
+    }
+}
+
+export const getTopics = () => async dispatch => {
+    const res = await csrfFetch('/api/stories/topics');
+
+    let data = await res.json();
+    if(res.ok){
+        dispatch(receiveTopics());
+    }else{
+        throw data;
+    }
+}
+
+export const getTopic = (topic) => async dispatch => {
+    const res = await csrfFetch(`/api/stories/topics/${id}`);
+
+    let data = await res.json();
+    if(res.ok){
+        dispatch(receiveTopic(topic));
+    }else{
+        throw data;
+    }
+}
+
+const topicReducer  = (state = {}, action) => {
+    let newState = {...state};
+
+    switch (action.type){
+        case RECEIVE_TOPICS:
+            return {...newState, ...action.topics};
+        case RECEIVE_TOPIC:
+            newState[action.topic.id] = action.topic
+            return newState;
+        case REMOVE_TOPIC:
+            delete newState[action.id]
+            return newState;
+        default: 
+            return state
+    }
+}
+
+export default topicReducer;
