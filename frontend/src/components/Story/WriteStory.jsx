84f95a959c62554426e5b1cd3dca995472bf1bc2
Tatianna <tatirobinson@gmail.com>
2023-12-07 09:09:03 -0800

    Create WriteStory component

---
+++ frontend/src/components/Story/WriteStory.jsx
@@ -0,0 +1,13 @@
+import Header from "../Header/Header";
+
+
+const WriteStory = () => {
+    return (
+        <>
+            <Header />
+            <h1> Write Story</h1>
+        </>
+    )
+}
+
+export default WriteStory;
