8da0804bac21edbd36e2d16f9758f6875964e582
Tatianna <tatirobinson@gmail.com>
2023-12-06 16:55:58 -0800

    Create routes, define and export components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,12 @@
 import {createBrowserRouter, Navigate, RouterProvider} from 'react-router-dom';
 import Home from './components/Home/Home';
-
+import StoryShow from './components/Story/StoryShow';
+import TopicIndex from './components/Topics/TopicIndex';
+import TopicShow from './components/Topics/TopicShow';
+import UserShow from './components/Users/UserShow';
 
 const router = createBrowserRouter([
+
   { 
     path: "/", 
     element: <Home />
@@ -11,6 +15,26 @@ const router = createBrowserRouter([
   {
     path: "*",
     element: < Navigate to="/" />
+  },
+
+  {
+    path: "/@:username",
+    element: <UserShow />
+  },
+
+  {
+    path: "/@:username/:storyTitle",
+    element: <StoryShow />
+  },
+
+  {
+    path: "/explore-topics",
+    element: <TopicIndex />
+  },
+
+  {
+    path: "/tag/:topicName",
+    element: <TopicShow />
   }
 ])
 
---
+++ frontend/src/components/Topics/TopicIndex.jsx
@@ -0,0 +1,7 @@
+
+
+const TopicIndex = () => {
+    
+}
+
+export default TopicIndex;
