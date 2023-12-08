f3245ae8f27cf3786edb5e6036565cdb1e381b96
Tatianna <tatirobinson@gmail.com>
2023-12-07 23:05:12 -0800

    Add modularized StoryList to TopicShow and UserShow components

--- frontend/src/components/Story/StoryList.jsx
+++ frontend/src/components/Story/StoryList.jsx
@@ -1,22 +1,10 @@
-import {useDispatch, useSelector} from 'react-redux';
-import { getStories } from '../../store/stories';
-import { useEffect } from 'react';
 import StoryPreview from './StoryPreview';
 
-
 const StoryList = ({stories}) => {
-    // const dispatch = useDispatch();
-    // const stories = useSelector(state => state.stories)
-
-    // useEffect( () => {
-    //     dispatch(getStories());
-    // }, [dispatch])
-
-    //console.log((stories));
 
     return(
         <>
-            <h1>Story List</h1>
+            <p>Story List</p>
              <ul>
                 {Object.values(stories).map(story => <StoryPreview key={story.id} story={story}/>)}
             </ul>
--- frontend/src/components/Topics/TopicShow.jsx
+++ frontend/src/components/Topics/TopicShow.jsx
@@ -1,12 +1,27 @@
+import { useParams } from "react-router-dom";
 import Header from "../Header/Header";
-
+import StoryList from "../Story/StoryList";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 
 const TopicShow = () => {
+    const {topicName} = useParams();
+
+    const stories = useSelector(state => state.stories);
+    const topics = useSelector(state => state.topics);
+
+    const currentTopic = Object.values(topics).find( topic => topic.name === topicName);
+    const topicStories = Object.values(stories).filter(story => story.topicId === currentTopic.id);
+    console.log("topicstories: ", topicStories);
+
     return (
         <>
             <Header/>
-            <h1>Topic Show</h1>
+            <p>Topic Show</p>
+            <h1>{topicName}</h1>
+            <StoryList stories={topicStories} />
+            <Link to="/explore-topics">See all Topics</Link>
         </>
     );
 
