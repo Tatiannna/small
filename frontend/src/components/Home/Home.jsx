93badbbcd3b05053af1c355893cd69e67600a120
Tatianna <tatirobinson@gmail.com>
2023-12-07 21:32:26 -0800

    Support modularized StoryList

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,10 +1,25 @@
 import Header from "../Header/Header";
+import StoryList from "../Story/StoryList";
 import './Home.css'
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getStories } from "../../store/stories";
 
 const Home = () => {
+    const dispatch = useDispatch();
+    const stories = useSelector(state => state.stories)
+
+    useEffect( () => {
+        dispatch(getStories());
+    }, [dispatch])
+
     return(
         <>
             <Header />
+            <Link to='/explore-topics'>Explore topics</Link>
+            <StoryList stories={stories} />
         </>
     )
 }
