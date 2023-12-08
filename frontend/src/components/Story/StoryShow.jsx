eb00e46c960f15a2d8f751c7cdf7970fd4114979
Tatianna <tatirobinson@gmail.com>
2023-12-07 21:31:42 -0800

    Fetch story data for StoryShow

--- frontend/src/components/Story/StoryShow.jsx
+++ frontend/src/components/Story/StoryShow.jsx
@@ -1,22 +1,35 @@
-import { useParams } from 'react-router-dom';
 import Header from '../Header/Header';
+import { useLocation } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getStory } from '../../store/stories';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 const StoryShow = () => {
-    //const {username} = useParams();
-    //const {storyTitle} = useParams();
-
-    //const stories = Object.values(useSelector(state => state.stories));
-    //const users = Object.values(useSelector(state => state.users));
-
-    //const story = stories.find( story => story.title === storyTitle && username === users[story.author_id].username);
-    //console.log(story);
     
-    // const { story } = useParams();  
+    const {username, storyTitle} = useParams();
+    const dispatch = useDispatch();
 
+    const stories = useSelector(state => state.stories);
+    const story = Object.values(stories).find( story => story.title === storyTitle);
+
+    useEffect(() => {
+        dispatch(getStory(story.id))
+    }, [dispatch, story.id]);
+
+    const topic = useSelector(state => state.topics[story.topicId]);
+    const author = useSelector(state => state.users[story.authorId]);
+    
     return(
         <>
             <Header/>
             <h1>Story Show</h1>
+            <h2>{story?.title}</h2>
+            <h4>{story?.detail}</h4>
+            <h5>{author?.username}</h5>
+            <p>{story?.body}</p>
+            <p>{topic?.name}</p>
         </>
     )
 }
