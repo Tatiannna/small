2cf2e40a86e076beea161469507c315c4ee62729
Tatianna <tatirobinson@gmail.com>
2023-12-07 21:30:07 -0800

    Make SoryList component modular

--- frontend/src/components/Story/StoryList.jsx
+++ frontend/src/components/Story/StoryList.jsx
@@ -1,23 +1,27 @@
 import {useDispatch, useSelector} from 'react-redux';
 import { getStories } from '../../store/stories';
 import { useEffect } from 'react';
-import StoryPreview from './StoryPreview'
+import StoryPreview from './StoryPreview';
 
 
-const StoryList = () => {
-    const dispatch = useDispatch();
-    const stories = useSelector(state => state.stories)
+const StoryList = ({stories}) => {
+    // const dispatch = useDispatch();
+    // const stories = useSelector(state => state.stories)
 
-    useEffect( () => {
-        dispatch(getStories());
-    }, [dispatch])
+    // useEffect( () => {
+    //     dispatch(getStories());
+    // }, [dispatch])
 
     //console.log((stories));
 
     return(
-        <ul>
-            {Object.values(stories).map(story => <StoryPreview key={story.id} story={story}/>)}
-        </ul>
+        <>
+            <h1>Story List</h1>
+             <ul>
+                {Object.values(stories).map(story => <StoryPreview key={story.id} story={story}/>)}
+            </ul>
+        </>
+        
     );
 }
 
