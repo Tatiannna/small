f31ecd3fb0295d9fe452681e1049ac46f675b8c9
Tatianna <tatirobinson@gmail.com>
2023-12-08 14:46:57 -0800

    Create stories#create

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -8,4 +8,21 @@ class Api::StoriesController < ApplicationController
         @story = Story.find_by(id: params[:id])
     end
 
+    def create
+        @story = Story.new(story_params)
+        if @story.save!
+            render :show
+        else
+            render json: @story.errors.full_messages, status: 422
+        end
+    end
+
+
+    private
+
+    def story_params
+        params.require(:story).permit(:title, :body, :detail, :author_id, :topic_id)
+    end
+
+
 end
