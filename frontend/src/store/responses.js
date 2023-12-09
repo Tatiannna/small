5f6e775759e103d43f7dfd795c805d35e1a4a662
Tatianna <tatirobinson@gmail.com>
2023-12-09 04:55:01 -0800

    Create Response reducer, action creators

--- frontend/src/store/responses.js
+++ frontend/src/store/responses.js
@@ -1,9 +1,28 @@
+import csrfFetch from "./csrf";
 
+const RECEIVE_RESPONSES = "/responses/RECEIVE_RESPONSES"
 
+const receiveResponses = (responses) => {
+    return {
+        type: RECEIVE_RESPONSES,
+        responses
+    }
+}
 
+const getResponses = (storyId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/stories/${storyId}/responses`)
+
+    let data = await res.json();
+    if (res.ok){
+        dispatch(receiveResponses(data));
+    }
+}
 
 const responseReducer = (state = {}, action) => {
+    let newState = {...state}
     switch(action.type){
+        case RECEIVE_RESPONSES:
+            return {...newState, ...action.responses}
         default:
             return state;
     }
