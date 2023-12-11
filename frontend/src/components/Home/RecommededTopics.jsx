aa922ec1b125b068f939231fd7cf7a6c9ed89a28
Tatianna <tatirobinson@gmail.com>
2023-12-11 09:23:39 -0800

    Make Topic Recommendation a component

---
+++ frontend/src/components/Home/RecommededTopics.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+
+const RecommendedTopics = () => {
+    return (
+        <>
+            <h4>Recommended Topics</h4>
+            <Link to='/tag/Photography'><button className='topic-button'>Photography</button></Link>
+            <Link to='/tag/Self-Help'><button className='topic-button'>Self-Help</button></Link>
+            <Link to='/tag/Data%20Science'><button className='topic-button'>Data Science</button></Link>
+            <Link to='/tag/Blockchain'><button className='topic-button'>BlockChain</button></Link>
+            <Link to='/tag/Movie%20Reviews'><button className='topic-button'>Movie Reviews</button></Link>
+            <Link to='/tag/Cryptocurrency'><button className='topic-button'>Cryptocurrency</button></Link>
+            <Link to="/explore-topics"><p className="see-more-topics">See More Topics</p></Link>
+        </>
+        
+    );
+}
+
+
+export default RecommendedTopics;
