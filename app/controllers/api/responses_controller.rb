79ed06cac0618726579f1e41179f0890a9e9c4d3
Tatianna <tatirobinson@gmail.com>
2023-12-10 19:06:34 -0800

    Add create action to Responses controller

--- app/controllers/api/responses_controller.rb
+++ app/controllers/api/responses_controller.rb
@@ -8,10 +8,27 @@ class Api::ResponsesController < ApplicationController
         @response = Response.find_by(id: params[:id])
     end
 
+    def create
+        @response = Response.new(response_params)
+
+        if @response.save!
+            @responses = [@response]
+            render :index
+        else
+            render json: @response.errors.full_messages, status: 422
+        end
+    end
+
     def update
     end
 
     def destroy
+    end
+
+    private
+
+    def response_params
+        params.require(:response).permit(:body, :story_id, :user_id )
     end
 
 end
