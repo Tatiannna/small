2aed508b2a9bdf35633da57d4422d20b6d13b106
Tatianna <tatirobinson@gmail.com>
2023-12-11 10:52:42 -0800

    Re-style Home after adding Topic Recommendations

--- frontend/src/components/Home/RecommendedTopics.jsx
+++ frontend/src/components/Home/RecommendedTopics.jsx
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom";
+import './RecommendedTopics.css';
 
 
 const RecommendedTopics = () => {
     return (
-        <>
+        <div className="recommended-topics-container">
             <h4>Recommended Topics</h4>
             <Link to='/tag/Photography'><button className='topic-button'>Photography</button></Link>
             <Link to='/tag/Self-Help'><button className='topic-button'>Self-Help</button></Link>
@@ -12,7 +13,7 @@ const RecommendedTopics = () => {
             <Link to='/tag/Movie%20Reviews'><button className='topic-button'>Movie Reviews</button></Link>
             <Link to='/tag/Cryptocurrency'><button className='topic-button'>Cryptocurrency</button></Link>
             <Link to="/explore-topics"><p className="see-more-topics">See More Topics</p></Link>
-        </>
+        </div>
         
     );
 }
