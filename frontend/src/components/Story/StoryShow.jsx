90cd8540fbfebf01c3a2902e067c163e413e6c3a
Tatianna <tatirobinson@gmail.com>
2023-12-10 19:07:11 -0800

    Edit styling

--- frontend/src/components/Story/StoryShow.jsx
+++ frontend/src/components/Story/StoryShow.jsx
@@ -1,5 +1,4 @@
 import Header from '../Header/Header';
-import { useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 import { getStory } from '../../store/stories';
@@ -7,6 +6,9 @@ import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import './StoryShow.css';
 import { Link } from 'react-router-dom';
+import { getResponses } from '../../store/responses';
+import ResponseModal from '../Responses/ResponseModal';
+import { useState } from 'react';
 
 const StoryShow = () => {
     
@@ -14,10 +16,15 @@ const StoryShow = () => {
     const dispatch = useDispatch();
 
     const stories = useSelector(state => state.stories);
+    const responses = useSelector(state => state.responses);
     const story = Object.values(stories).find( story => story.title === storyTitle);
 
+    const [showResponseModal, setShowResponseModal] = useState(false);
+
     useEffect(() => {
-        dispatch(getStory(story.id))
+        dispatch(getStory(story.id));
+        dispatch(getResponses(story.id));
+
     }, [dispatch, story.id]);
 
     const topic = useSelector(state => state.topics[story.topicId]);
@@ -26,23 +33,27 @@ const StoryShow = () => {
     return(
         <>
             <Header/>
-            <div class="story-container">
-                <p class="story-show-title">{story?.title}</p>
-                <p class="story-show-detail">{story?.detail}</p>
-                <div class="story-show-info-container">
-                    <div class="story-show-info-container-left">
-                        <span class="story-show-avatar">&#9824; </span>
+            <div className="story-container">
+                <p className="story-show-title">{story?.title}</p>
+                <p className="story-show-detail">{story?.detail}</p>
+                <div className="story-show-info-container">
+                    <div className="story-show-info-container-left">
+                        <span className="story-show-avatar">&#9824; </span>
                     </div>
-                    <div class="story-show-info-container-right">
-                        <Link to=''> <p class="story-show-username">{author?.username}</p></Link>
-                        <p class="story-show-info">5 min read <span className="dot">&#x2022; </span>Nov 14</p>
+                    <div className="story-show-info-container-right">
+                        <Link to=''> <p className="story-show-username">{author?.username}</p></Link>
+                        <p className="story-show-info">5 min read <span className="dot">&#x2022; </span>Nov 14</p>
                     </div>
                 </div>
-                <div className="divider"></div>
-                <p class="story-show-body">{story?.body}</p>
-                <Link to=''><span class="story-show-topic">{topic?.name}</span></Link>
+                <div className="divider">
+                    {showResponseModal && < ResponseModal story={story} closeModal={() => setShowResponseModal(false)} />}
+                    <span className="Comments" onClick={()=> setShowResponseModal(!showResponseModal)}>Comments</span>
+                </div>
+                <p className="story-show-body">{story?.body}</p>
+                <Link to=''><span className="story-show-topic">{topic?.name}</span></Link>
             </div>
         </>
     )
 }
+
 export default StoryShow;
