29d5b5482d9c97d284044dc160e41ba6089c3ece
Tatianna <tatirobinson@gmail.com>
2023-12-12 15:46:21 -0800

    Restructure preview elements, add menu to each preview

--- frontend/src/components/Story/StoryPreview.jsx
+++ frontend/src/components/Story/StoryPreview.jsx
@@ -5,6 +5,8 @@ import { useSelector } from 'react-redux';
 import { getTopic } from '../../store/topics';
 import { useEffect } from 'react';
 import { getUser } from '../../store/users';
+import { useState } from 'react';
+import StoryMenu from './StoryMenu';
 
 
 const StoryPreview = (props) => {
@@ -13,12 +15,13 @@ const StoryPreview = (props) => {
     const dispatch = useDispatch();
 
     useEffect( () => {
-        dispatch(getTopic(story.topicId));
-        dispatch(getUser(story.authorId))
-    }, [dispatch, story.topicId])
+        dispatch(getTopic(story?.topicId));
+        dispatch(getUser(story?.authorId))
+    }, [dispatch, story?.topicId])
 
-    const author = useSelector( state => state.users[story.authorId]);
-    const topic = useSelector(state => state.topics[story.topicId]);
+    const author = useSelector( state => state.users[story?.authorId]);
+    const topic = useSelector(state => state.topics[story?.topicId]);
+    const [showPreviewMenu, setShowPreviewMenu] = useState(false);
     
     return (
         <div className="story-preview">
@@ -29,18 +32,20 @@ const StoryPreview = (props) => {
                 </p>
             </Link>
             <Link to={`/${author?.username}/${story?.title}`}>
-                <p className="preview-title">{story.title}</p>
-                <p className="preview-detail">{story.detail}</p>
+                <p className="preview-title">{story?.title}</p>
+                <p className="preview-detail">{story?.detail}</p>
             </Link>
-            <Link to={`/tag/${topic?.name}`}>
-                <p>
-                    <span className="preview-date">Dec 4</span>
-                    <span className="dot"> &#x2022; </span> 
-                    <span className="preview-time">5 min read </span>
-                    <span className="dot">&#x2022; </span> 
-                    <span className="preview-topic"> {topic?.name}</span>
-                </p>
-            </Link>
+            <p>
+                <span className="preview-date">Dec 4</span>
+                <span className="dot"> &#x2022; </span> 
+                <span className="preview-time">5 min read </span>
+                <span className="dot">&#x2022; </span> 
+                <Link to={`/tag/${topic?.name}`}>
+                    <span className="preview-topic">{topic?.name}</span>
+                </Link>
+                <span className="preview-menu" onClick={() => setShowPreviewMenu(!showPreviewMenu)}>...</span>
+                {showPreviewMenu && <StoryMenu story={story}/>}
+            </p>
         </div>
     );
 }
