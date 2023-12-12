6ffaedc53e808101a500ed7a5ead4306e0652836
Tatianna <tatirobinson@gmail.com>
2023-12-12 15:35:27 -0800

    add update and destroy actions to Stories controller

--- app/controllers/api/stories_controller.rb
+++ app/controllers/api/stories_controller.rb
@@ -17,6 +17,26 @@ class Api::StoriesController < ApplicationController
         end
     end
 
+    def destroy
+        @story = Story.find_by(id: params[:id])
+
+        if @story && @story.destroy
+            head :no_content
+        else
+            render json: ['Something went wrong'], status: 422
+        end
+    end
+
+    def update
+        @story = Story.find_by(id: params[:id]) 
+
+        if @story.update(story_params)
+            render :show
+        else
+            render json: ['Something went wrong'], status: 422
+        end
+    end
+
 
     private
 
