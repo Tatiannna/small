61dfa723cd43e1d9a5ee06cae4330aed45ce4020
Tatianna <tatirobinson@gmail.com>
2023-12-13 11:42:52 -0800

    Edit styling, show signup modal on button click

--- frontend/src/components/Home/StayCurious.jsx
+++ frontend/src/components/Home/StayCurious.jsx
@@ -1,16 +1,22 @@
-import './StayCurious.css'
+import './StayCurious.css';
+import Modal from '../Modal/Modal';
+import { useState } from 'react';
 
 
 const StayCurious = () => {
 
+    const [showModal, setShowModal] = useState(false);
+
     return (
-        <div className="stay-curious">
-            <h1>Stay Curious.</h1>
-            <h3>Discover Stories, thinking, and
-                expertise from writers on any topic.</h3>
-            <button>Start Reading</button>  
-        </div>
-        
+        <>
+            {showModal && (<Modal closeModal={() => setShowModal(false)} formType='signup' />)}
+            <div className="stay-curious">
+                <h1>Stay Curious.</h1>
+                <h3>Discover Stories, thinking, and
+                    expertise from writers on any topic.</h3>
+                <button className='start-reading' onClick={()=>setShowModal(true)}>Start reading</button>  
+            </div>
+        </>
     );
 }
 
