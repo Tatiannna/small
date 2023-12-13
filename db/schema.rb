8ba14ef8aa4e6b179cc8bb412ff44f85b949c341
Tatianna <tatirobinson@gmail.com>
2023-12-12 16:22:37 -0800

    Allow no topic for Stories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_12_09_094459) do
+ActiveRecord::Schema[7.1].define(version: 2023_12_12_050022) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -30,7 +30,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_12_09_094459) do
     t.string "title", null: false
     t.bigint "author_id", null: false
     t.text "body", null: false
-    t.bigint "topic_id", null: false
+    t.bigint "topic_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.text "detail"
